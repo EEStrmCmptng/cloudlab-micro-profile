97173e0826fec089522b64d69dbeebbd84e39404
hand32 <hand32@node0.hand32-220044.bayopsys-pg0.apt.emulab.net>
2024-09-26 14:29:26 -0600

    testing

--- profile.py
+++ profile.py
@@ -10,18 +10,49 @@ Wait for the profile instance to start, then click on the node in the topology a
 import geni.portal as portal
 # Import the ProtoGENI library.
 import geni.rspec.pg as pg
+# Emulab extensions.
+import geni.rspec.emulab as emulab
 
 # Create a portal context.
 pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
 request = pc.makeRequestRSpec()
- 
+
+# Default ubuntu image
+ubuntu_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD'
+
+pc.defineParameter("dataset", "Your dataset URN",
+                   portal.ParameterType.STRING,
+                   "urn:publicid:IDN+apt.emulab.net:bayopsys-pg0+imdataset+EEFlinkMicro")
+
+pc.defineParameter("mountpoint", "Mountpoint for file system",
+                   portal.ParameterType.STRING, "/mydata")
+
+pc.defineParameter("phystype",
+                    "Physical Node Type",
+                    portal.ParameterType.STRING,
+                    "c6220")
+
+pc.defineParameter("sameSwitch",  "No Interswitch Links", portal.ParameterType.BOOLEAN, False,
+                    advanced=True,
+                    longDescription="Sometimes you want all the nodes connected to the same switch. " +
+                    "This option will ask the resource mapper to do that, although it might make " +
+                    "it imppossible to find a solution. Do not use this unless you are sure you need it!")
+
+# Retrieve the values the user specifies during instantiation.
+params = pc.bindParameters()
+pc.verifyParameters()
+
 # Add a raw PC to the request.
-node = request.RawPC("node")
+node = request.RawPC("mynode")
+node.disk_image = ubuntu_image
+node.hardware_type = params.phystype
+node_bs = node.Blockstore("bs", params.mountpoint)
+node_bs.dataset = params.dataset
 
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
+node.addService(pg.Execute(shell="bash", command="/local/repository/silly.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
