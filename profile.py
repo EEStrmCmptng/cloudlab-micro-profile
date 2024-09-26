e86eec5977b16fab6492e603a2efbabefc5c3c9a
hand32 <hand32@node0.hand32-220044.bayopsys-pg0.apt.emulab.net>
2024-09-26 15:11:09 -0600

    testing

--- profile.py
+++ profile.py
@@ -34,6 +34,13 @@ pc.defineParameter("phystype",
                     portal.ParameterType.STRING,
                     "c6220")
 
+# Optional link speed, normally the resource mapper will choose for you based on node availability
+pc.defineParameter("linkSpeed", "Link Speed",portal.ParameterType.INTEGER, 0,
+                   [(0,"Any"),(100000,"100Mb/s"),(1000000,"1Gb/s"),(10000000,"10Gb/s"),(25000000,"25Gb/s"), (40000000,"40Gb/s"), (100000000,"100Gb/s")],
+                   advanced=True,
+                   longDescription="A specific link speed to use for your lan. Normally the resource " +
+                   "mapper will choose for you based on node availability and the optional physical type.")
+
 pc.defineParameter("sameSwitch",  "No Interswitch Links", portal.ParameterType.BOOLEAN, False,
                     advanced=True,
                     longDescription="Sometimes you want all the nodes connected to the same switch. " +
@@ -44,15 +51,36 @@ pc.defineParameter("sameSwitch",  "No Interswitch Links", portal.ParameterType.B
 params = pc.bindParameters()
 pc.verifyParameters()
 
-# Add a raw PC to the request.
-node = request.RawPC("mynode")
-node.disk_image = ubuntu_image
-node.hardware_type = params.phystype
-node_bs = node.Blockstore("bs", params.mountpoint)
-node_bs.dataset = params.dataset
+# IP [10.10.1.1, 10.10.1.2, 10.10.1.3, 10.10.1.4]
+#JobManager10-1, Source10-2, Mapper10-3, Sink10-4
+
+# Create link/lan.
+lan = request.LAN()
+if params.linkSpeed > 0:
+    lan.bandwidth = params.linkSpeed
+if params.sameSwitch:
+    lan.setNoInterSwitchLinks()
+    
+# JobManager node
+node1 = request.RawPC("JobManager10-1")
+node1.disk_image = ubuntu_image
+node1.hardware_type = params.phystype
+node1_bs = node1.Blockstore("bs", params.mountpoint)
+node1_bs.dataset = params.dataset
+iface1 = node1.addInterface()
+iface1.addAddress(rspec.IPv4Address("10.10.1.1", "255.255.255.0"))
+lan.addInterface(iface1)
+    
+# Source node
+node2 = request.RawPC("Source10-2")
+node2.disk_image = ubuntu_image
+node2.hardware_type = params.phystype
+iface2 = node2.addInterface()
+iface2.addAddress(rspec.IPv4Address("10.10.1.2", "255.255.255.0"))
+lan.addInterface(iface2)
 
 # Install and execute a script that is contained in the repository.
-node.addService(pg.Execute(shell="bash", command="/local/repository/silly.sh"))
+#node1.addService(pg.Execute(shell="bash", command="/local/repository/silly.sh"))
 
 # Print the RSpec to the enclosing page.
 pc.printRequestRSpec(request)
